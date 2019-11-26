c9ea2a4620122cbe2351ac8ecbc60ecbb2ed15e4
krtomo <krrtomo@gmail.com>
2019-11-26 18:47:02 +0900

    add columns to users

--- db/migrate/20191126080319_devise_create_users.rb
+++ db/migrate/20191126080319_devise_create_users.rb
@@ -4,11 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :name, null: false, default: "", unique: true
+      t.string :email, null: false, default: "", unique: true
+      t.time :this_travel_time
+      t.time :total_travel_time
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
@@ -32,11 +35,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
