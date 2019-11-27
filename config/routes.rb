0355c6a112b92550f3aac89bf42afbcb7086f28c
MamiKosaka <m.kosaka.0228@gmail.com>
2019-11-27 17:43:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  resources :users, only: [:index, :edit, :update, :index, :show]
+  resources :users, only: [:index, :edit, :update, :show]
 end
