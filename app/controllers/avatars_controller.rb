98db706ca7810f60540a813587da73a6e6e31c21
MamiKosaka <m.kosaka.0228@gmail.com>
2019-11-28 15:48:08 +0900

    try_error_new_avatar

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,5 +1,5 @@
 class AvatarsController < ApplicationController
-
+  protect_from_forgery except: :create
   def new
     @avatar = Avatar.new
   end
@@ -16,6 +16,7 @@ class AvatarsController < ApplicationController
   private
   def avatar_params
     params.require(:avatar).permit(:name, :type,:stage,:curr_station_id, :last_station_id, :home_station_id)
+    # .merge(user_id: current_user.id)
   end
 
 end
