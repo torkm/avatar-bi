07d34cceb8641220c3c15fe098ca58c7c7665e24
MamiKosaka <m.kosaka.0228@gmail.com>
2019-12-02 16:37:59 +0900

    rgst_new_avatars_lat_and_long_w_jname

--- app/assets/javascripts/avatar.js
+++ app/assets/javascripts/avatar.js
@@ -1,7 +1,6 @@
 $(window).on('load', function(){
   $(function(){
     
-
     function select_AvatarType(avatar_type){
       let html = `<input value="${avatar_type}" type="hidden" name="avatar[avatar_type]" id="avatar_avatar_type">`;
       $('#avatar_avatar_type').remove();
@@ -9,32 +8,41 @@ $(window).on('load', function(){
     }
 
     function buildSelectHTML(stations){
-      let html              = `<form name="home_station_form">
+      $('#dummy_div').remove();
+      let html              = `<form name="home_station_form" id="home_station_form">
                                 <select name="home_station_select" id="home_station_select">
                                   <option value="0" disabled="disabled">駅を選んでください</option>`
       $.each(stations,function(index, station){
-        let each_station_html = `<option value="${station.id}">${station.name}駅  (${station.railway_name})</option>`
+        let each_station_html = `<option value="${station.id}">${station.name}駅  (${station.railway_jname})</option>`
         html += each_station_html;
       });
       html += ` </select>
-                <input type="text" name="dummy" id="hidden"/>
-                <input type="button" onclick="submit();" id="hidden"/>
+                <div name="dummy_div">
+                  <input type="text" name="dummy" id="hidden"/>
+                  <input type="button" onclick="submit();" id="hidden"/>
+                </div>
               </form>`
-      $('#home_station_select').remove();
+      $('#home_station_form').remove();
       $('.form__home-station-input').append(html);
       $('#home_station_select').val(0);
     };
 
-
-    function buildSendHome_Station_ID(home_station_id){
+    function buildSendHome_Station_ID(home_station){
       $('#avatar_home_station_id').remove();
       $('#avatar_curr_station_id').remove();
       $('#avatar_last_station_id').remove();
-      let html = `<input value="${home_station_id}" type="hidden" name="avatar[home_station_id]" id="avatar_home_station_id">
-                  <input value="${home_station_id}" type="hidden" name="avatar[curr_station_id]" id="avatar_curr_station_id">
-                  <input value="${home_station_id}" type="hidden" name="avatar[last_station_id]" id="avatar_last_station_id">`;
+      let html = `<input value="${home_station.id}" type="hidden" name="avatar[home_station_id]" id="avatar_home_station_id">
+                  <input value="${home_station.id}" type="hidden" name="avatar[curr_station_id]" id="avatar_curr_station_id">
+                  <input value="${home_station.id}" type="hidden" name="avatar[last_station_id]" id="avatar_last_station_id">
+                  <input value="${home_station.lat}" type="hidden" name="avatar[curr_location_lat]" id="avatar_curr_location_lat">
+                  <input value="${home_station.lat}" type="hidden" name="avatar[last_location_lat]" id="avatar_last_location_lat">
+                  <input value="${home_station.long}" type="hidden" name="avatar[curr_location_long]" id="avatar_curr_location_long">
+                  <input value="${home_station.long}" type="hidden" name="avatar[last_location_long]" id="avatar_last_location_long">
+                  `;
       $('.form__home-station-input').append(html);
     }
+
+    let candidate_stations = [];
 
     $('#home_station_input-field').on("keyup", function(e){
       if($('#home_station_input-field').val()!==""){
@@ -50,6 +58,7 @@ $(window).on('load', function(){
         .done(function(stations){
           if(stations[0] !== undefined){
             let html = buildSelectHTML(stations);
+            candidate_stations = stations;
           };
         });
       } else {
@@ -58,33 +67,37 @@ $(window).on('load', function(){
     });
 
     $('.form__home-station-input').change('#home_station_select', function(){
+
       let home_station_id = $('#home_station_select').val();
       if(home_station_id !== null){
         let input = $('option:selected').text();
         $('#home_station_input-field').val(input);
         $('#home_station_select').remove();
-        buildSendHome_Station_ID(home_station_id);
+        let home_station = candidate_stations.filter(function(station, index){
+          if ( station.id === Number(home_station_id) ){return true};
+        }); 
+        buildSendHome_Station_ID(home_station[0]);
       };
     });
 
-    $('.form__type-select__img--1').on("click", function(e){
+    $('.form__type-select__op__img--1').on("click", function(e){
       e.preventDefault();
-      $('.form__type-select__img--selected').removeClass('form__type-select__img--selected');
-      $(this).addClass('form__type-select__img--selected');
+      $('.form__type-select__op__img--selected').removeClass('form__type-select__op__img--selected');
+      $(this).addClass('form__type-select__op__img--selected');
       select_AvatarType(1);
     });
 
-    $('.form__type-select__img--2').on("click", function(e){
+    $('.form__type-select__op__img--2').on("click", function(e){
       e.preventDefault();
-      $('.form__type-select__img--selected').removeClass('form__type-select__img--selected');
-      $(this).addClass('form__type-select__img--selected');
+      $('.form__type-select__op__img--selected').removeClass('form__type-select__op__img--selected');
+      $(this).addClass('form__type-select__op__img--selected');
       select_AvatarType(2);
     });
 
-    $('.form__type-select__img--3').on("click", function(e){
+    $('.form__type-select__op__img--3').on("click", function(e){
       e.preventDefault();
-      $('.form__type-select__img--selected').removeClass('form__type-select__img--selected');
-      $(this).addClass('form__type-select__img--selected');
+      $('.form__type-select__op__img--selected').removeClass('form__type-select__op__img--selected');
+      $(this).addClass('form__type-select__op__img--selected');
       select_AvatarType(3);
     });
   });
--- app/views/stations/index.json.jbuilder
+++ app/views/stations/index.json.jbuilder
@@ -2,7 +2,7 @@ json.array! @stations do |station|
   json.id station.id
   json.name station.name
   json.railway_id station.railway_id
-  json.railway_name station.railway.name
+  json.railway_jname station.railway.jname
   json.lat station.lat
   json.long station.long
 end
