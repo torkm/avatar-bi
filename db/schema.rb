3b3dfa3732f1e534c1e4f5cdc15127b050ef5f93
krtomo <krrtomo@gmail.com>
2019-12-03 16:58:07 +0900

    changed type from time into int

--- db/schema.rb
+++ db/schema.rb
@@ -74,8 +74,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_153356) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
-    t.time "this_travel_time"
-    t.time "total_travel_time"
+    t.integer "this_travel_time"
+    t.integer "total_travel_time"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
