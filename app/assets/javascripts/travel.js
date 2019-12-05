a573726003eb237609d664341265f1133db2183d
krtomo <krrtomo@gmail.com>
2019-12-05 12:16:17 +0900

    インデント調整

--- app/assets/javascripts/travel.js
+++ app/assets/javascripts/travel.js
@@ -1,30 +1,30 @@
-$(window).on('load', function(){
-  $(function(){
-    if (document.URL.match('/')){
+$(window).on('load', function () {
+  $(function () {
+    if (document.URL.match('/')) {
 
-      function change_btn(is_moving){
-        if(is_moving){
-          $('.start_end_btn').data('btn','end');
+      function change_btn(is_moving) {
+        if (is_moving) {
+          $('.start_end_btn').data('btn', 'end');
           $('.start_end_btn').val('移動終了');
-        }else{
-          $('.start_end_btn').data('btn','start');
-          $('.start_end_btn').val('移動開始');  
+        } else {
+          $('.start_end_btn').data('btn', 'start');
+          $('.start_end_btn').val('移動開始');
         };
       }
 
-      function avatar_traveling(){
+      function avatar_traveling() {
         $.ajax({
           type: "GET",
           url: "users/reload_user",
           dataType: "json"
         })
-        .done(function(user){
-          console.log(user.is_moving);
-          change_btn(user.is_moving);
-        })
+          .done(function (user) {
+            console.log(user.is_moving);
+            change_btn(user.is_moving);
+          })
       }
-      
-      setInterval(avatar_traveling, 1000);
+
+      setInterval(avatar_traveling, 60000);
     };
   });
 })
