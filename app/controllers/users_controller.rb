4b8dae486f947d162c5585c4e2c7b9f9c4633d9f
MamiKosaka <m.kosaka.0228@gmail.com>
2019-12-06 16:52:09 +0900

    crr_comment_out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
     if user_signed_in?
       gon.avatars = current_user.avatars
       gon.current_user = current_user
-      # gon.global.curr_location_lat = current_user.avatars[0].curr_location_lat
-      # gon.global.curr_location_long = current_user.avatars[0].curr_location_long
+      gon.global.curr_location_lat = current_user.avatars[0].curr_location_lat
+      gon.global.curr_location_long = current_user.avatars[0].curr_location_long
     end
   end
 
