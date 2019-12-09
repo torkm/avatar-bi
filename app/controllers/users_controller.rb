98dfd5c72f340cfcb265488ae209fb5bef742ea9
krtomo <krrtomo@gmail.com>
2019-12-09 19:03:28 +0900

    ユーザー登録後初回の移動を可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ require "time"
 class UsersController < ApplicationController
   def index
     if user_signed_in?
+      # csvなかった場合、作成
+      unless File.exist?("db/csv/#{current_user.avatars[0].id}_curr.csv")
+        a_id = current_user.avatars[0].id
+        sta = Station.find(Avatar.find(a_id).home_station_id)
+        CSV.open("db/csv/#{current_user.id}_curr.csv", "w") do |content|
+          content << [sta.id, sta.odpt_sameAs, sta.name, sta.railway.jname, sta.lat, sta.long, sta.id, sta.name, 0, ""]
+          # 現在駅id, 現在駅sameAs, 現在駅名, 現在路線,　現在lat, 現在long, 次駅id, 次駅名, 進行方向の角度, 現在時刻表
+        end
+      end
       gon.avatars = current_user.avatars
       gon.current_user = current_user
     end
