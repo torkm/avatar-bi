613d0e1406d4245ba2c17ed18af40af60ef59af6
MamiKosaka <m.kosaka.0228@gmail.com>
2019-12-10 12:38:52 +0900

    set_fr_in_html

--- app/assets/javascripts/travel.js
+++ app/assets/javascripts/travel.js
@@ -3,37 +3,30 @@ $(window).on('load', function () {
     if (document.URL.match('/')) {
 
       function avatar_traveling() {
-        $.ajax({
-          type: "GET",
-          url: "/users/reload",
-          dataType: "json"
-        })
-          .done(function (user) {
-            if (user.is_moving) {
-              console.log("is_moving");
-              var url = "/avatars/travel"
+        if ($('#is_moving').length) {
+          console.log("is_moving");
+          var url = "/avatars/travel"
+          $.ajax({
+            type: "GET",
+            url: url,
+            dataType: "json"
+          }) // 場所の確認
+            .done(function () {
               $.ajax({
                 type: "GET",
-                url: url,
+                url: "/avatars/reload",
                 dataType: "json"
-              }) // 場所の確認
-                .done(function () {
-                  $.ajax({
-                    type: "GET",
-                    url: "/avatars/reload",
-                    dataType: "json"
-                  })
-                    .done(function (avatar_info) {
-                      $('#test_curr_timetable').text(avatar_info.timetable);
-                      $('#test_curr_station').text(avatar_info.sta_name);
-                      $('#test_curr_location_lat').text(avatar_info.curr_lat);
-                      $('#test_curr_location_long').text(avatar_info.curr_long);
-                    });
+              })
+                .done(function (avatar_info) {
+                  $('#test_curr_timetable').text(avatar_info.train_timetable);
+                  console.log(avatar_info.train_timetable);
+                  $('#test_curr_station').text(avatar_info.sta_name);
+                  $('#test_curr_location_lat').text(avatar_info.curr_lat);
+                  $('#test_curr_location_long').text(avatar_info.curr_long);
                 });
-            };
-          });
-      };
-
+            });
+        };
+      }
       setInterval(avatar_traveling, 3000);
     };
   });
