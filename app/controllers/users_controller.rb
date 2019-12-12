ef24f3f11d9d1ed6f878e068882a8539e9bb6045
krtomo <krrtomo@gmail.com>
2019-12-12 13:26:44 +0900

    アバターの進化とランキングの表示機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,18 +13,11 @@ class UsersController < ApplicationController
 
         unless File.exist?("db/csv/#{avatar.id}_curr.csv")
           sta = Station.find(avatar.home_station_id)
-          CSV.open("db/csv/#{current_user.id}_curr.csv", "w") do |content|
+          CSV.open("db/csv/#{avatar.id}_curr.csv", "w") do |content|
             content << [sta.id, sta.odpt_sameAs, sta.name, sta.railway.jname, sta.lat, sta.long, sta.id, sta.name, 0, ""]
-            # 現在駅id, 現在駅sameAs, 現在駅名, 現在路線,　現在lat, 現在long, 次駅id, 次駅名, 進行方向の角度, 現在時刻表
+            # 現在駅id, 現在駅sameAs, 現在駅名, 現在路線, 現在lat, 現在long, 次駅id, 次駅名, 進行方向の角度, 現在時刻表
           end
         end
-        # passed_stationsのためのcsv 現在は使わない予定
-        # unless File.exist?("db/csv/#{current_user.id}_#{avatar.id}_record.csv")
-        #   CSV.open("db/csv/#{current_user.id}_#{avatar.id}_record.csv", "w") do |content|
-        #     content << [0,"","",0,"",0,0,Time.now]
-        #     # 駅id, 駅sameAs, 駅名, 路線id, 路線名, 通過回数, 最新到着時刻
-        #   end
-        # end
 
         gon.avatars = current_user.avatars
         # 1-9でアイコンタイプを指定
@@ -46,9 +39,8 @@ class UsersController < ApplicationController
     avatar = current_user.avatars[0]
     # 複数ならここから下ループ
 
-    # 全駅数と路線数は計算量節約のため定数で
+    # 全駅数と路線数は計算量節約のため定数で(下が計算式)
     @total_st, @total_rw = 1173, 62
-
     # @total_rw = Railway.where(has_TrainTimetable: true).count  #全路線数
     # Railway.where(has_TrainTimetable: true).each do |r|
     #   @total_st += r.station_num  #各路線の総駅数を足す
@@ -81,6 +73,29 @@ class UsersController < ApplicationController
         end
       end
     end
+
+    # アバターのレベルを判定してアイコン表示  画像ファイルの番号も指定
+    if @comp_st < 200
+      avatar.stage = 1
+    elsif @comp_st < 1000
+      avatar.stage = 2
+    else
+      avatar.stage = 3
+    end 
+    avatar.save
+    @avatar_type = (avatar.avatar_type + 3*(avatar.stage-1)).to_s 
+
+    # ランキングを表示
+    h = User.order("total_travel_time DESC").pluck(:id)
+    @rank_time = h.index(avatar.id) + 1
+
+    h = PassedStation.group(:avatar_id).count
+    h = Hash[ h.sort_by{ |_, v| -v } ]
+    @rank_station = h.keys.index(avatar.id) + 1
+
+    h = PassedRailway.group(:avatar_id).count
+    h = Hash[ h.sort_by{ |_, v| -v } ]
+    @rank_railway = h.keys.index(avatar.id) + 1
 
     # 地図を表示 [路線名, 路線id, 駅名, lat, long, has_passed, passed_at]を返す
     gon.avatar = avatar
