0c588d0cb1a5f420998aadca78c84efa08d7bd13
krtomo <krrtomo@gmail.com>
2019-12-17 14:42:28 +0900

    移動中のコメント表示

--- app/assets/javascripts/travel.js
+++ app/assets/javascripts/travel.js
@@ -18,6 +18,19 @@ $(window).on('load', function () {
                 dataType: "json"
               })
                 .done(function (avatar_info) {
+                  if (avatar_info.sta_name == avatar_info.n_sta_name) {
+                    // 現在駅==次駅 「終点につきました」
+                    $('#test_curr_condition').text(`電車が終点${avatar_info.sta_name}駅につきました。次の電車を探しています。`);
+                  } else {
+                    console.log(eval(avatar_info.timetable)[0][1])
+                    // 電車は決まったがまだ乗っていない 「x駅で◯線を待っている」
+
+                    // 駅着時間+10秒以内は「◯◯駅にいます」
+
+                    //駅間走行中 「x駅とy駅の間を走っている」
+                    $('#test_curr_condition').text(`${new Date()}現在、${avatar_info.sta_name}駅から${avatar_info.n_sta_name}駅に${avatar_info.railway}で移動中`);
+                  };
+
                   $('#test_curr_timetable').text(avatar_info.timetable);
                   $('#test_curr_station').text(avatar_info.sta_name);
                   $('#test_curr_location_lat').text(avatar_info.curr_lat);
