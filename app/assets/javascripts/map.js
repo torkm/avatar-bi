11a6295ac72653fbe0085424916db24714ff4129
krtomo <krrtomo@gmail.com>
2019-12-18 12:27:18 +0900

    Update map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -303,7 +303,7 @@ $(function () {
         content: getNow()
       },
       icon: {
-        url: `../assets/avatar_type${gon.icon_type}.png`, //アイコンの画像パス
+        url: `avatar_type${gon.icon_type}.png`, //アイコンの画像パス
         scaledSize: {
           width: 50,
           height: 50
@@ -333,7 +333,7 @@ $(function () {
           content: `${val[0]}${val[2]}駅 / ${val[5]}回通過 (最新:${val[6]})`
         },
         icon: {
-          url: `../assets/${val[0]}.png`, //アイコンの画像パス
+          url: `${val[0]}.png`, //アイコンの画像パス
           scaledSize: {
             width: 26,
             height: 27
