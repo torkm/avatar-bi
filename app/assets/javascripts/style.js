582d4e34e800cd7c5194193f592dea3ddb41b004
MamiKosaka <m.kosaka.0228@gmail.com>
2019-12-19 12:50:32 +0900

    add_detail_stations

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -9,8 +9,26 @@ $(function () {
     $('.detail_show').addClass('indivi');
   });
 
-  // $('.detail_plus').on('click', function () {
-  //   $('.detail_stations').removeClass(indivi);
-  // });
+  $("[class^=detail_plus_]").on("click", function () {
+    var plus_name = $(this).attr("class");
+    var minus_name = plus_name.replace("detail_plus_", ".detail_minus_");
+
+    $(this).addClass('indivi');
+    $(minus_name).removeClass('indivi');
+
+    var detail_station_name = plus_name.replace("detail_plus_", ".detail_stations_");
+    $(detail_station_name).removeClass('indivi');
+  });
+
+  $("[class^=detail_minus_]").on("click", function () {
+    var minus_name = $(this).attr("class");
+    var plus_name = minus_name.replace("detail_minus_", ".detail_plus_");
+
+    $(this).addClass('indivi');
+    $(plus_name).removeClass('indivi');
+
+    var detail_station_name = minus_name.replace("detail_minus_", ".detail_stations_");
+    $(detail_station_name).addClass('indivi');
+  });
 
 })
