829a2e2c9ef8be07f9c32ec2bd9dc03cbdb5c188
MamiKosaka <m.kosaka.0228@gmail.com>
2019-12-20 18:42:27 +0900

    cannot_edit_user_unless_current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -152,6 +152,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # binding.pry
+    # unless current_user.id == params[:id].to_i
+    #   redirect_to root_path
+    # end
   end
 
   def update
